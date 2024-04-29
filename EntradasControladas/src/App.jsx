8bca41739c8a0a1491f0371fe2eddd119c54942c
JojiLoverx <martinezjaureguijavier@cbtis116.edu.mx>
2024-04-29 15:19:24 -0700

    first commit

--- EntradasControladas/src/App.jsx
+++ EntradasControladas/src/App.jsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function App (){
+  const [miTexto, setMiTexto]=useState('');
 
-function App() {
-  const [count, setCount] = useState(0)
+  const handleText = (Event)=> {
+    const textoIngresado = event.target.value;
+    setMiTexto(textoIngresado);
+  };
+  return(
+    <div>
+      <div>Ingrese Texto: </div>
+      <input type="text" onChange={handleText} value={miTexto} />
+      <p>{miTexto}</p>
+    </div>
+  );
+ }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
